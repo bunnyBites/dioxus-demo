106524950af2e76fdf481c61ae2a5ab92394d12c
Bunny Bites <s.mukeshkr481@gmail.com>
2023-11-04 14:02:06 +0530

    prepare field values during submit

--- src/components/login_form.rs
+++ src/components/login_form.rs
@@ -1,11 +1,33 @@
 use dioxus::prelude::*;
+use log::info;
+use serde::{Deserialize, Serialize};
 
 use crate::components::input::Input;
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct User {
+    username: String,
+    password: String,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct UserObj {
+    username: Vec<String>,
+    password: Vec<String>,
+}
 
 pub fn LoginForm(cx: Scope) -> Element {
     let onsubmit = |event: Event<FormData>| {
         event.stop_propagation();
-        log::info!("{:?}", event.values);
+        let value_str: String = serde_json::to_string(&event.values).unwrap();
+        let user_object: UserObj = serde_json::from_str(&value_str).unwrap();
+
+        let prepared_user = User {
+            username: user_object.username.into_iter().next().expect("The vector was empty"),
+            password: user_object.password.into_iter().next().expect("The vector was empty"),
+        };
+
+        info!("{:?}", prepared_user);
     };
 
     render!(
